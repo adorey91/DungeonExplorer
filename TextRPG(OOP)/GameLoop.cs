540db3fc30c0aac07b801d51f99e428b33872f13
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2024-01-24 18:46:57 -0400

    started work on map

--- TextRPG(OOP)/GameLoop.cs
+++ TextRPG(OOP)/GameLoop.cs
@@ -14,33 +14,37 @@ namespace TextRPG_OOP_
         static Map gameMap;
         static void Main(string[] args)
         {
+            StartUp();
+            Console.WriteLine("Welcome to my Text RPG game!");
+            Console.WriteLine();
+            //Testing v
+            // Console.WriteLine("The players HP: " + mainPlayer.healthSystem.health);
+            // Console.WriteLine("The Enemies HP: " + firstEnemy.healthSystem.health);
+            // mainPlayer.healthSystem.TakeDamage(firstEnemy.enemyDamage);
+            // Console.WriteLine(string.Format("{0} took {1} damage from {2}", mainPlayer.playerName, firstEnemy.enemyDamage,firstEnemy.enemyName));
+            // firstEnemy.healthSystem.TakeDamage(mainPlayer.playerDamage);
+            // Console.WriteLine(string.Format("{0} took {1} damage from {2}", firstEnemy.enemyName, mainPlayer.playerDamage,mainPlayer.playerName));
+            // Console.WriteLine("The players HP: " + mainPlayer.healthSystem.health);
+            // Console.WriteLine("The Enemies HP: " + firstEnemy.healthSystem.health);
+            // mainPlayer.healthSystem.Heal(500, mainPlayer.PlayerMaxHP);
+            // Console.WriteLine(string.Format("{0} gained 500, but they only have {1} max HP.",mainPlayer.playerName, mainPlayer.PlayerMaxHP));
+            // firstEnemy.healthSystem.Heal(1000, firstEnemy.enemyMaxHP);
+            // Console.WriteLine(string.Format("{0} gained 1000, but they only have {1} max HP.",firstEnemy.enemyName, firstEnemy.enemyMaxHP));
+            // Console.WriteLine("The players HP: " + mainPlayer.healthSystem.health);
+            // Console.WriteLine("The Enemies HP: " + firstEnemy.healthSystem.health);
+            // Console.WriteLine(string.Format("{0} has a position of {1}X : {2}Y",mainPlayer.playerName, mainPlayer.position.x,mainPlayer.position.y));
+            // Console.WriteLine(string.Format("{0} has a position of {1}X : {2}Y",firstEnemy.enemyName, firstEnemy.position.x, firstEnemy.position.y));
+            gameMap.DrawMap(gameMap.activeMap);
+            //Testing ^
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey(true);
+        }
+        static void StartUp()
+        {
             gameMap = new Map();
             mainPlayer = new Player();
             firstEnemy = new Enemy();
             firstEnemy.enemyName = "Slime";
-            Console.WriteLine("Welcome to my Text RPG game!");
-            Console.WriteLine();
-            //Testing v
-            Console.WriteLine("The players HP: " + mainPlayer.healthSystem.health);
-            Console.WriteLine("The Enemies HP: " + firstEnemy.healthSystem.health);
-            mainPlayer.healthSystem.TakeDamage(firstEnemy.enemyDamage);
-            Console.WriteLine(string.Format("{0} took {1} damage from {2}", mainPlayer.playerName, firstEnemy.enemyDamage,firstEnemy.enemyName));
-            firstEnemy.healthSystem.TakeDamage(mainPlayer.playerDamage);
-            Console.WriteLine(string.Format("{0} took {1} damage from {2}", firstEnemy.enemyName, mainPlayer.playerDamage,mainPlayer.playerName));
-            Console.WriteLine("The players HP: " + mainPlayer.healthSystem.health);
-            Console.WriteLine("The Enemies HP: " + firstEnemy.healthSystem.health);
-            mainPlayer.healthSystem.Heal(500, mainPlayer.PlayerMaxHP);
-            Console.WriteLine(string.Format("{0} gained 500, but they only have {1} max HP.",mainPlayer.playerName, mainPlayer.PlayerMaxHP));
-            firstEnemy.healthSystem.Heal(1000, firstEnemy.enemyMaxHP);
-            Console.WriteLine(string.Format("{0} gained 1000, but they only have {1} max HP.",firstEnemy.enemyName, firstEnemy.enemyMaxHP));
-            Console.WriteLine("The players HP: " + mainPlayer.healthSystem.health);
-            Console.WriteLine("The Enemies HP: " + firstEnemy.healthSystem.health);
-            Console.WriteLine(string.Format("{0} has a position of {1}X : {2}Y",mainPlayer.playerName, mainPlayer.position.x,mainPlayer.position.y));
-            Console.WriteLine(string.Format("{0} has a position of {1}X : {2}Y",firstEnemy.enemyName, firstEnemy.position.x, firstEnemy.position.y));
-            //Testing ^
-            Console.WriteLine(gameMap);
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey(true);
         }
     }
 }
--- TextRPG(OOP)/Map.cs
+++ TextRPG(OOP)/Map.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TextRPG_OOP_
 {
@@ -12,9 +13,191 @@ namespace TextRPG_OOP_
         string path1 = @"Floor1Map.txt";
         string path2 = @"Floor2Map.txt";
         string path3 = @"Floor3Map.txt";
-        string[] floorMap;
-        public char[,] dungeonMap;
+        public string[] floorMap;
+        public char[,] activeMap;
         int levelNumber;
         bool levelChanged;
+        public char dungeonFloor = ((char)18);
+        public char dungeonWall = ((char)35);
+        public char spikeTrap = ((char)23);
+        public char player = ((char)2);
+        public char stairsDown = ((char)30);
+        public char stairsUp = ((char)31);
+        public char finalLoot = ((char)165);
+        public char coin = ((char)164);
+        public char healthPickup = ((char)3);
+        int mapX;
+        int mapY;
+        public Map() //Constructor
+        {
+            Initialization();
+        }
+        public void Initialization()
+        {
+            path = path1;
+            floorMap = File.ReadAllLines(path);
+            activeMap = new char[floorMap.Length, floorMap[0].Length];
+            MakeDungeonMap();
+        }
+        public void DrawMap(char[,] dungeonMap)
+        {
+            //Draws the map of the current level
+            Console.SetCursorPosition(0,0);
+            for(int y = 0; y < mapY; y++)
+            {
+                for(int x = 0; x < mapX; x++)
+                {
+                    char tile = dungeonMap[y,x];
+                    DrawTile(tile);     
+                }
+                Console.Write("\n");
+            }
+            Console.SetCursorPosition(0,0);
+        }
+        public void MakeDungeonMap()
+        {
+            for (int i = 0; i < floorMap.Length; i++)
+            {
+                for (int j = 0; j < floorMap[i].Length; j++)
+                {
+                    activeMap[i, j] = floorMap[i][j];
+                }
+            } 
+        }
+        void DrawFloor()
+        {
+            // used to draw a floor tile
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Write(dungeonFloor);
+            SetColorDefault();
+        }
+        void DrawWall()
+        {
+            // used to draw a wall tile
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Write(dungeonWall);
+            SetColorDefault();
+        }
+        void DrawSpikes()
+        {
+            // used to draw a spikes tile
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Write(spikeTrap);
+            SetColorDefault();
+        }
+        void DrawFinalLoot()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Write(finalLoot);
+            SetColorDefault();
+        }
+        void DrawCoin()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Write(coin);
+            SetColorDefault();
+        }
+        void SetColorDefault()
+        {
+            // sets console color back to default. 
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+        void DrawStairsDown()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Write(stairsDown);
+            SetColorDefault();
+        }
+        void DrawHealthPickup()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Write(healthPickup);
+            SetColorDefault();
+        }
+        void DrawPlayer(int PosX, int PosY)
+        {
+            // used to draw the player
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Write(player);
+            SetColorDefault();
+        }
+        void DrawTile(Char tile)
+        {
+            // draws the correct tile based on the floorMap
+            if(tile == '-')
+            {
+                DrawFloor();
+                return;
+            }
+            if(tile == '#')
+            {
+                DrawWall();
+                return;
+            }
+            if(tile == '*')
+            {
+                DrawSpikes();
+                return;
+            }
+            if(tile == '~')
+            {
+                DrawStairsDown();
+                return;
+            }
+            if(tile == '=')
+            {
+                DrawWall();
+                return;
+            }
+            if(tile == '$')
+            {
+                DrawFinalLoot();
+                return;
+            }
+            if(tile == '@')
+            {
+                DrawCoin();
+                return;
+            }
+            if(tile == '"')
+            {
+                DrawHealthPickup();
+                return;
+            }
+            if(tile == '!')
+            {
+                DrawFloor();
+                return;
+            }
+            if(tile == '?')
+            {
+                DrawFloor();
+                return;
+            }
+            if(tile == '&')
+            {
+                DrawFloor();
+                return;
+            }
+            if(tile == '^')
+            {
+                DrawFloor();
+                return;
+            }
+            else
+            {
+                Console.Write(tile);
+            }
+        }
     }
+
 }
